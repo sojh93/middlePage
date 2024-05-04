2789796df9074955a12c32cb84e908889d6964e1
JeongHyeonSo <36619557+sojh93@users.noreply.github.com>
2024-05-04 16:38:54 +0900

    뷰설치. indexts에서 선언 파일 못찾는다는 에ㅓㄹ 발생중.

---
+++ middlepage/src/views/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>부제</div>
+    <div></div>
+</template>
+
+<style scoped>
+</style>
+
+<script>
+import { ref } from 'vue'
+
+const data_1 = ref([
+    { id: 1, img: require(''), text: '' },
+    { id: 2, img: '', text: '' },
+])
+</script>
